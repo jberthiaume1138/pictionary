d41112f1d2d92605c530a1eeaf937d2e1a4ea104
jberthiaume1138 <joenyc23@gmail.com>
2016-08-15 01:25:49 -0400

    add mousedown/up functionality

--- public/main.js
+++ public/main.js
@@ -14,15 +14,26 @@ var pictionary = function() {
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
+
+    var drawing;
+
+    canvas.on('mousedown', function(event) {
+        drawing = true;
+    });
+
+    canvas.on('mouseup', function(event) {
+        drawing = false;
+    });
+
     canvas.on('mousemove', function(event) {
-        var offset = canvas.offset();
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-        draw(position);
+        if(drawing === true) {
+            var offset = canvas.offset();
+            var position = {x: event.pageX - offset.left,
+                            y: event.pageY - offset.top};
+            draw(position);
 
-        socket.emit('draw', position);
-
-
+            socket.emit('draw', position);
+        }
     });
 
     socket.on('draw', draw);
